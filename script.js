3574fa82aea4e7c8290f190f979fb97ad0569d35
atsntsgo <tsnarsen@gmail.com>
2024-12-07 17:46:49 +0200

    added basic number input and clearing

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ document.querySelectorAll('.button-layout button').forEach(button => {
     document.addEventListener('mouseup', removeClicked);
   });
 });
+
+/* button press logic */
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const value = button.getAttribute('data-value');
+    const action = button.getAttribute('data-action');
+    console.log(value)
+
+    if (action === 'clear') {
+      display.textContent = '0'; // Reset the display
+    } else {
+      if (display.textContent === '0') {
+        display.textContent = value; // Replace the initial "0"
+      } else {
+        display.textContent += value; // Append the new character
+      }
+    }
+  });
+});
